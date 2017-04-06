9fe1065f42cf15e6eb2f195ff9bd1a5168d7ea49
smallbomb <rockon590+git@gmail.com>
2017-04-06 22:03:00 +0800

    Hello world
    Init

---
+++ DiscordBot/MyBot.cs
@@ -0,0 +1,53 @@
+﻿using Discord;
+using Discord.Commands;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot
+{
+    class MyBot
+    {
+        DiscordClient discord;
+        public MyBot()
+        {
+            discord = new DiscordClient(x =>
+            {
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+
+            });
+
+            discord.UsingCommands(x =>
+            {
+                x.PrefixChar = '!';
+                x.AllowMentionPrefix = true;
+            });
+
+            var commands = discord.GetService<CommandService>();
+
+            commands.CreateCommand("hello")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("Hi!");
+                });
+
+
+
+
+            discord.ExecuteAndWait(async () =>
+            {
+                await discord.Connect("Mjk5NDkzMzgxODYyNzE5NDg4.C8etnQ.EYxxj8QFIDC5dlPhycjs8dViq9Q", TokenType.Bot);
+            });
+        }
+
+        private void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine(e.Message);
+
+        }
+    }
+}
