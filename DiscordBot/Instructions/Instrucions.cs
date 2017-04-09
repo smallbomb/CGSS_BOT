abda27273f65650f1db3d6fabec73f8b2f1e438e
Agreerga <Agreerga@hotmail.com>
2017-04-09 19:31:58 +0800

    #fix-issue 指令說明

--- DiscordBot/Instructions/Instrucions.cs
+++ DiscordBot/Instructions/Instrucions.cs
@@ -5,20 +5,50 @@ namespace DiscordBot.Instructions
 {
     public class Instrucions : MyBot
     {
+        public static string[] BotAnswer = { "Yes", "Maybe", "No"};
+        // 隨機種子產生器
         public static Random random = new Random(Guid.NewGuid().GetHashCode());
+        // ========================== 說明指令集 ===============================
+        public static void ResponseMessage_Helper(string messages, MessageEventArgs e)
+        {
+            switch (messages)
+            {
+                case "!Agr_Hello":
+                    {
+                        e.Message.Channel.SendMessage("```\nHello World.\nThis is a debug arg.\n```");
+                    }
+                    break;
+                case "!Agr_卯月":
+                    {
+                        e.Message.Channel.SendMessage("```\nShow Uzuki QQ Picture.\nThis is a debug arg.\n```");
+                    }
+                    break;
+                case "!Agr_gatya":
+                    {
+                        e.Message.Channel.SendMessage("```\nRandom Pick Number(s)\nFor Range 0 ~ 100, Maxium pick is 10.\nUsage: !Agr_gatya 10\n```");
+                    }
+                    break;
+                case "!Agr_ask":
+                    {
+                        e.Message.Channel.SendMessage("```\nRandom Answer The Question.\nUsage: !Agr_ask Qes1\n```");
+                    }
+                    break;
+            }
+        }
+        // =====================================================================
         // ======================== 單一字詞指令集 =============================
         public static void ResponseMessage_Single(string messages, MessageEventArgs e)
         {
             switch (messages)
             {
-                case "!hello2":
+                case "!Agr_Hello":
                     {
-                        e.Message.Channel.SendMessage("World!");
+                        e.Message.Channel.SendMessage("`World!`");
                     }
                     break;
-                case "!卯月":
+                case "!Agr_卯月":
                     {
-                        e.Message.Channel.SendFile("../../envelope/QQ.jpg");
+                        e.Message.Channel.SendFile("`../../envelope/QQ.jpg`");
                     }
                     break;
                 default:
@@ -29,9 +59,15 @@ namespace DiscordBot.Instructions
         // ====================== 夾帶單一參數指令集 ===========================
         public static void ResponseMessage_Double(string messages, string param, MessageEventArgs e)
         {
+            // Help 說明指令
+            if(param == "help")
+            {
+                ResponseMessage_Helper(messages, e);
+                return;
+            }
             switch (messages)
             {
-                case "!gatya":
+                case "!Agr_gatya":
                     {
                         if (Convert.ToInt32(param) > 11) return;
                         int randNum = 0;
@@ -41,7 +77,13 @@ namespace DiscordBot.Instructions
                             randNum = random.Next(100);
                             output += randNum.ToString() + " ";
                         }
-                        e.Message.Channel.SendMessage(output);
+                        e.Message.Channel.SendMessage("`" + output + "`");
+                    }
+                    break;
+                case "!Agr_ask":
+                    {
+                        string output = e.Message.User + " : " + param + " ?\nAnswer Is : " + BotAnswer[random.Next(3)];
+                        e.Message.Channel.SendMessage("`" + output + "`");
                     }
                     break;
                 default:
@@ -88,7 +130,7 @@ namespace DiscordBot.Instructions
             {
                 if (e.Channel.Type == ChannelType.Text && !e.Channel.IsPrivate)
                 {
-                    e.Channel.SendMessage("一個新頻道已經建立，說點話吧!!");
+                    e.Channel.SendMessage("`一個新頻道已經建立，說點話吧!!`");
                 }
             };
         }
