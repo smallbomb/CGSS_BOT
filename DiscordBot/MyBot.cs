ef83ec681340936dcd21493bd1bb77dedc4afb61
Agreerga <Agreerga@hotmail.com>
2017-05-02 20:43:49 +0800

    加入線上新增歌曲資料庫功能

--- DiscordBot/MyBot.cs
+++ DiscordBot/MyBot.cs
@@ -67,6 +67,7 @@ namespace DiscordBot
             Draw.DrawCardCommand();
             Instrucions.SetInstrucions();
             Lots.DrawLotsCommand() ;
+            PickSong.SetSongs();
                 ;
                 ;
             /*
